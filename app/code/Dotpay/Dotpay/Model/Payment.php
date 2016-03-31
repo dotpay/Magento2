8e44336fb1eed1a841018ab54302a34af57cbddf
sylwek <sylwek@pc224466.localhost>
2016-03-31 14:22:47 +0200

    add oneclick

--- app/code/Dotpay/Dotpay/Model/Payment.php
+++ app/code/Dotpay/Dotpay/Model/Payment.php
@@ -38,6 +38,18 @@ class Payment extends \Magento\Payment\Model\Method\AbstractMethod implements Co
         , 'SEK'
         , 'DKK'
     );
+    
+    protected  $_resource;
+    
+    protected $_deploymentConfig;
+    
+    protected $_connection;
+    
+    protected $_tablePrefix;
+    
+    protected $_tableOneClick = 'dotpay_oneclick';
+    
+    protected $_checkTableOneClick = false;
 
     public function __construct(
         \Magento\Framework\Model\Context $context,
@@ -51,7 +63,10 @@ class Payment extends \Magento\Payment\Model\Method\AbstractMethod implements Co
         \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate,
         \Magento\Directory\Model\CountryFactory $countryFactory,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\App\ResourceConnection $resource,
+        \Magento\Framework\App\DeploymentConfig $deploymentConfig,
         array $data = array()
+        
     ) {
         parent::__construct(
             $context,
@@ -72,6 +87,17 @@ class Payment extends \Magento\Payment\Model\Method\AbstractMethod implements Co
         $this->_maxAmount = $this->getConfigData('max_order_total');
         
         $this->_storeManager = $storeManager;
+        
+        $this->_resource = $resource;
+        $this->_deploymentConfig = $deploymentConfig;
+        
+        /**
+         * One-Click
+         */
+        $this->getDBConnection();
+        $this->getDBTablePrefix();
+        $this->createTableOneClick();
+        $this->_checkTableOneClick = $this->checkTableOneClick();
     }
 
     /**
@@ -253,5 +279,236 @@ class Payment extends \Magento\Payment\Model\Method\AbstractMethod implements Co
      */
     public function disableAgreements() {
         $this->_agreements = false;
+    }
+    
+    /**
+     * 
+     */
+    private function getDBConnection() {
+        if(!isset($this->_connection)) {
+            $this->_connection = $this->_resource->getConnection(
+                \Magento\Framework\App\ResourceConnection::DEFAULT_CONNECTION
+            );
+        }
+    }
+    
+    /**
+     * 
+     */
+    private function getDBTablePrefix() {
+        if(!isset($this->_tablePrefix)) {
+            $this->_tablePrefix = $this->_deploymentConfig->get(
+                \Magento\Framework\Config\ConfigOptionsListConstants::CONFIG_PATH_DB_PREFIX
+            );
+        }
+    }
+    
+     /**
+     * 
+     */
+    private function createTableOneClick() {
+        
+        $sqlCreateTable = <<<END
+            CREATE TABLE IF NOT EXISTS `{$this->_tablePrefix}{$this->_tableOneClick}` (
+                `oneclick_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                `oneclick_order` bigint(20) NOT NULL,
+                `oneclick_user` bigint(20) NOT NULL,
+                `oneclick_card_title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+                `oneclick_card_hash` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+                `oneclick_card_id` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+                PRIMARY KEY (`oneclick_id`),
+                UNIQUE KEY `oneclick_card_hash` (`oneclick_card_hash`),
+                UNIQUE KEY `oneclick_order` (`oneclick_order`),
+                UNIQUE KEY `oneclick_card_id` (`oneclick_card_id`),
+                KEY `oneclick_user` (`oneclick_user`),
+                KEY `oneclick_card_title` (`oneclick_card_title`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1;
+
+END;
+            
+        try {
+            $this->_connection->rawQuery($sqlCreateTable);
+        } catch (Exception $exc) {
+           /**
+            * NOP
+            */
+        }
+    }
+    
+    /**
+     * 
+     */
+    private function checkTableOneClick() {
+        $ok = false;
+        
+        $results = $this->_connection->fetchAll('SHOW TABLES');
+        foreach ($results as $tables) {
+            foreach ($tables as $table) {
+                if($table === "{$this->_tablePrefix}{$this->_tableOneClick}") {
+                   $ok = true;
+                   break 2;
+                }
+            }
+        }
+        
+        return $ok;
+    }
+    
+    /**
+     * 
+     */
+    public function getConnection() {
+        return $this->_connection;
+    }
+    
+    /**
+     * 
+     */
+    public function getTablePrefix() {
+        return $this->_tablePrefix;
+    }
+    
+    /**
+     * 
+     */
+    public function cardList($user) {
+        
+        $sql = <<<END
+            SELECT *
+            FROM {$this->_tablePrefix}{$this->_tableOneClick}
+            WHERE
+                oneclick_user = '{$user}'
+                AND
+                oneclick_card_id IS NOT NULL
+            ;
+
+END;
+            $results = array();
+            if($this->_checkTableOneClick) {
+                $results = $this->_connection->fetchAll($sql);
+            }
+            
+            return $results;
+    }
+    
+    /**
+     * 
+     */
+    private function generateCardHash() {
+        $microtime = '' . microtime(true);
+        $md5 = md5($microtime);
+        
+        $mtRand = mt_rand(0, 11);
+        
+        $md5Substr = substr($md5, $mtRand, 21);
+        
+        $a = substr($md5Substr, 0, 6);
+        $b = substr($md5Substr, 6, 5);
+        $c = substr($md5Substr, 11, 6);
+        $d = substr($md5Substr, 17, 4);
+        
+        return "{$a}-{$b}-{$c}-{$d}";
+    }
+    
+    /**
+     * 
+     */
+    public function cardAdd($orderId, $userId, $cardTitle) {
+        $result = 0;
+        
+        if($this->_checkTableOneClick) {
+            $count = 100;
+            do {
+                $cardHash = $this->generateCardHash();
+                $test = $this->_connection->insert(
+                    "{$this->_tablePrefix}{$this->_tableOneClick}"
+                    , array( 
+                        'oneclick_order' => "{$orderId}"
+                        ,'oneclick_user' => "{$userId}"
+                        ,'oneclick_card_title' => "{$cardTitle}" 
+                        ,'oneclick_card_hash' => "{$cardHash}" 
+                    )
+                    , array(
+                        '%d'
+                        ,'%d'
+                        ,'%s'
+                        ,'%s'
+                    )
+                );
+                
+                if(false !== $test) {
+                    $result = $cardHash;
+                    break;
+                }
+                
+                $count--;
+            } while($count);
+        }
+            
+        return $result;
+    }
+    
+    /**
+     * 
+     */
+    public function cardGetIdByCardHash($user, $cardHash) {
+        $sql = <<<END
+            SELECT *
+            FROM {$this->_tablePrefix}{$this->_tableOneClick}
+            WHERE
+                oneclick_user = '{$user}'
+                AND
+                oneclick_card_hash = '{$cardHash}'
+            LIMIT 1
+            ;
+
+END;
+            
+            $results = null;
+            if($this->_checkTableOneClick) {
+                $row = $this->_connection->fetchRow($sql);
+                $results = isset($row['oneclick_id']) ? $row['oneclick_id'] : null;
+            }
+            
+            return $results;
+    }
+    
+    /**
+     * 
+     */
+    public function cardGetHashByOrderId($user, $orderId) {
+        $sql = <<<END
+            SELECT *
+            FROM {$this->_tablePrefix}{$this->_tableOneClick}
+            WHERE
+                oneclick_user = '{$user}'
+                AND
+                oneclick_order = '{$orderId}'
+            LIMIT 1
+            ;
+
+END;
+            
+            $results = null;
+            if($this->_checkTableOneClick) {
+                $row = $this->_connection->fetchRow($sql);
+                $results = isset($row['oneclick_card_hash']) ? $row['oneclick_card_hash'] : null;
+            }
+            
+            return $results;
+    }
+    
+    /**
+     * 
+     */
+    public function cardRegister() {
+        
+    }
+    
+    /**
+     * 
+     */
+    public function cardDel() {
+        
     }
 }
